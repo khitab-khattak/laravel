d3fd177159fd43a04a6b4f417131cfecbf73cb4d
Khitab-khattakgit commit -m first <khitabk29@gmail.com>
2025-03-13 05:53:48 +0500

    belongs to and has many

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -11,5 +11,9 @@ use Illuminate\Database\Eloquent\Model;
         use HasFactory;
     protected $table = 'job_listings';
     protected $fillable = ['title','salary'];
+
+    public function employer(){
+        return $this->belongsTo(Employer::class);
+    }
     }
 
